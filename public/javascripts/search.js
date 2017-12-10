05e1b442b5df8738df1158f8e87897945af455ea
Dranderson <dranderson@DrandersondeMacBook-Air.local>
2017-12-09 22:52:49 -0600

    add naive view for search

---
+++ public/javascripts/search.js
@@ -0,0 +1,11 @@
+function initMap() {
+	var uluru = {lat: 30.2672, lng: -97.7431};
+	var map = new google.maps.Map(document.getElementById('map'), {
+	  zoom: 12,
+	  center: uluru
+	});
+	var marker = new google.maps.Marker({
+	  position: uluru,
+	  map: map
+	});
+}
